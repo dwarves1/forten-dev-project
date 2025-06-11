e03ccf8238517d7eca4cf9ea41498e86b8a3b47e
hnayoung <hnaa12@daum.net>
2025-06-11 11:40:02 +0900

    useLayoutEffect 사용해 렌더링 시 깜빡임 방지 및 의존성 location.key로 변경(ScrollToTop)

--- frontend/src/components/common/ScrollToTop.jsx
+++ frontend/src/components/common/ScrollToTop.jsx
@@ -1,11 +1,13 @@
-import { useEffect } from "react";
+import { useLayoutEffect } from "react";
 import { useLocation } from "react-router-dom";
 
 export default function ScrollToTop() {
-  const { pathname } = useLocation();
+  const location = useLocation();
 
-  useEffect(() => {
+  // useEffect -> useLayoutEffect 변경
+  // 화면 렌더링 전에 스크롤 이동 처리해서 깜빡임 방지
+  useLayoutEffect(() => {
     window.scrollTo(0, 0);
-  }, [pathname]);
+  }, [location.key]);
   return null;
 }
